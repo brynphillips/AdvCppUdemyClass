8e0ef6139dde2273d68f4f3396e5460969521e6a
brynphillips <brynphillips1@gmail.com>
2018-11-07 11:43:16 -0800

    Just some updates.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Second change" << std::endl;
+
+
+    cout << "This is going to be change numbber 3";
+
+
     return 0;
 }
