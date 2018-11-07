f06c75bc1917224aa0b95a8ff5a42d68639afa3e
brynphillips <brynphillips1@gmail.com>
2018-11-07 12:19:23 -0800

    Lesson 2

--- main.cpp
+++ main.cpp
@@ -2,42 +2,69 @@
 
 using namespace std;
 
+//      ****Lesson 1*****
+//void mightGoWrong() {
+//    bool error1 = false;
+//    bool error2 = false;
+//    bool error3 = true;
+//
+//    if(error1) {
+//        throw "Something went wrong";
+//    }
+//    if (error2) {
+//        throw string("Something else went wrong.");
+//    }
+//    if (error3) {
+//        throw
+//    }
+//}
+//
+//void usesMightgoWrong() {
+//    mightGoWrong();
+//}
 
-
-void mightGoWrong() {
-    bool error1 = false;
-    bool error2 = true;
-
-    if(error1) {
-        throw "Something went wrong";
+//      ****Lesson 2****
+class CanGoWrong {
+public:
+    CanGoWrong() {
+        char *pMemory = new char[999999999999999];
+        delete[] pMemory;
     }
-    if (error2) {
-        throw string("Something else went wrong.");
-    }
-}
 
-void usesMightgoWrong() {
-    mightGoWrong();
-}
+};
 
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+//    ***Lesson 1***
+//    std::cout << "Hello, World!" << std::endl;
+//
+//    try {
+//        usesMightgoWrong();
+//    }
+//    catch(int e){
+//        cout << "Error code " << e << endl;
+//    }
+//    catch (const char *e){
+//        cout << "Error message: " << e << endl;
+//    }
+//    catch (string &e) {
+//        cout << "String error message: " << e << endl;
+//    }
+//
+//    cout << "Still running" << endl;
+
+//    ***Lesson 2***
 
     try {
-        usesMightgoWrong();
+        CanGoWrong wrong;
     }
-    catch(int e){
-        cout << "Error code " << e << endl;
-    }
-    catch (const char *e){
-        cout << "Error message: " << e << endl;
-    }
-    catch (string &e) {
-        cout << "String error message: " << e << endl;
+    catch (bad_alloc &e) {
+        cout << "Caught excpetion: " <<  e.what() << endl;
     }
 
-    cout << "Still running" << endl;
+    cout << "Still running";
+
 
     return 0;
 }
