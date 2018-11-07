24dc5d068e98f8ea466c95425c6f113a7779fcc3
brynphillips <brynphillips1@gmail.com>
2018-11-07 11:59:05 -0800

    Lesson 1

--- main.cpp
+++ main.cpp
@@ -2,17 +2,30 @@
 
 using namespace std;
 
+void mightGoWrong() {
+    bool error = true;
+
+    if(error) {
+        throw "Something went wrong";
+    }
+}
+
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Second change" << std::endl;
 
+    try {
+        mightGoWrong();
+    }
+    catch(int e){
+        cout << "Error code " << e << endl;
+    }
+    catch (const char *e){
+        cout << "Error message: " << e << endl;
+    }
 
-    cout << "This is going to be change number 3";
-    cout << "This might be different.";
-
-
+    cout << "Still running" << endl;
 
     return 0;
 }
